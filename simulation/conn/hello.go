6539bb251c5522523c8bd93b1225594369c1d130
Alonza0314 <a0984078318@gmail.com>
2024-08-25 02:52:15 +0800

    update via git

--- simulation/conn/hello.go
+++ simulation/conn/hello.go
@@ -1,13 +1,32 @@
 package conn
 
-import "crypto/ed25519"
+import (
+	"crypto/ed25519"
+	"net"
+)
 
 var CLIENT_HELLO string = "clientHello"
 var SERVER_HELLO string = "serverHello"
 
 type Request struct {
-	hello string
-	publicKey	ed25519.PublicKey
-	options map[string]string
+	hello     string
+	publicKey ed25519.PublicKey
+	options   map[string]string
+}
+type Reply struct {
+	hello     string
+	publicKey ed25519.PublicKey
+	options   map[string]string
 }
 
+func NewRequest() (*Request, error) {
+
+}
+
+func NewReply() (*Reply, error) {
+	
+}
+
+func (r *Request) SendForReply(conn net.Conn) []byte {
+
+}
